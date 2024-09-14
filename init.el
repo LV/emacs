ae18fef703ca7e5fd809c9a13972ee27ac3e6c50
Luis Victoria <v@lambda.lv>
2024-09-14 18:14:58 -0400

    Add package sources

--- init.el
+++ init.el
@@ -14,3 +14,21 @@
 (set-face-attribute 'default nil :font "PragmataPro Mono Liga" :height 180)
 
 (load-theme 'tango-dark)
+
+;; Initialize package sources
+(require 'package)    ; Brings in the package management functions into the environment
+
+(setq package-archives '(("melpa" . "https://melpa.org/packages/")
+			 ("org" . "https://orgmode.org/elpa/")
+			 ("elpa" . "https://elpa.gnu.org/packages/")))
+
+(package-initialize)  ; Initialize the package system and prepares it to be used
+(unless package-archive-contents  ; refresh contents only if packages don't exist
+  (package-refresh-contents))
+
+;; Initialize use-package on non-Linux platforms
+(unless (package-installed-p 'use-package)
+  (package-install 'use-package))
+
+(require 'use-package)
+(setq use-package-always-ensure t)
