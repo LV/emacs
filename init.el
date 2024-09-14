7130d99924b4aabe4e368ddac2453baa0f0b2c03
Luis Victoria <v@lambda.lv>
2024-09-14 17:24:21 -0400

    Add fringe of size 10

--- init.el
+++ init.el
@@ -3,3 +3,4 @@
 (scroll-bar-mode -1) ; Disable visible scrollbar
 (tool-bar-mode -1)   ; Disable toolbar
 (tooltip-mode -1)    ; Disable tooltips
+(set-fringe-mode 10) ; Add fringe space to the left and right of the buffer
