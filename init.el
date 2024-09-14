20174c74b87e247354defabad8c56e78a329e25d
Luis Victoria <v@lambda.lv>
2024-09-14 17:31:29 -0400

    Enable sound bell
    This is due to using MacOS

--- init.el
+++ init.el
@@ -1,6 +1,12 @@
 (setq inhibit-startup-message t)
 
-(scroll-bar-mode -1) ; Disable visible scrollbar
-(tool-bar-mode -1)   ; Disable toolbar
-(tooltip-mode -1)    ; Disable tooltips
-(set-fringe-mode 10) ; Add fringe space to the left and right of the buffer
+(scroll-bar-mode -1)    ; Disable visible scrollbar
+(tool-bar-mode -1)      ; Disable toolbar
+(tooltip-mode -1)       ; Disable tooltips
+(set-fringe-mode 10)    ; Add fringe space to the left and right of the buffer
+
+(menu-bar-mode 1)       ; Enable the menu bar
+
+(setq visible-bell nil) ; If true, replaces sound bell for visual cue when performing action that can't be done
+                        ;   (e.g. backspacing on the first line of the document, down arrow on last line, etc)
+                        ; On MacOS, this makes a large caution triangle which can be very annoying, so perhaps disable if using Mac
