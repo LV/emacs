18d504fac206b10f09b1c154d0207b3ae8a1c6f3
Luis Victoria <v@lambda.lv>
2024-09-15 00:21:39 -0400

    Fix bug with Evil not loading by removing hook

--- init.el
+++ init.el
@@ -43,18 +43,6 @@
 (require 'use-package)
 (setq use-package-always-ensure t)
 
-(defun lv/evil-hook ()
-  (dolist (mode '(custom-mode
-		  eshell-mode
-		  git-rebase-mode
-		  erc-mode
-		  circe-server-mode
-		  circe-chat-mode
-		  circe-query-mode
-		  sauron-mode
-		  term-mode))
-    (add-to-list 'evil-emacs-state-modes mode)))
-
 ;; Vim Mode
 (use-package evil
   :ensure t
@@ -63,7 +51,6 @@
   (setq evil-want-keybinding nil)
   ; (setq evil-want-C-u-scroll t)
   ; (setq evil-want-C-i-jump nil)
-  :hook (evil-mode . lv/evil-hook)
   :config
   (evil-mode 1) ; turn on Evil mode
   ; (define-key evil-insert-state-map (kbd "C-g") 'evil-normal-state) ; I think this is the same as just hitting escape when you're not in insert mode
