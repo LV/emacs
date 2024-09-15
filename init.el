dd13ef92347753cd0680e710153eb48d32198f4d
Luis Victoria <v@lambda.lv>
2024-09-14 20:19:47 -0400

    Add rainbow delimiters

--- init.el
+++ init.el
@@ -77,6 +77,9 @@
   :init (doom-modeline-mode 1)
   :custom (doom-modeline-height 15))
 
+(use-package rainbow-delimiters
+  :hook (prog-mode . rainbow-delimiters-mode))
+
 (custom-set-variables
  ;; custom-set-variables was added by Custom.
  ;; If you edit it by hand, you could mess it up, so be careful.
