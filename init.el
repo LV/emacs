3df4428ef73b22201bf95b0c42b9bf1d950e9c59
Luis Victoria <v@lambda.lv>
2024-09-14 23:35:11 -0400

    Expand configuration for Evil

--- init.el
+++ init.el
@@ -43,14 +43,38 @@
 (require 'use-package)
 (setq use-package-always-ensure t)
 
+(defun lv/evil-hook ()
+  (dolist (mode '(custom-mode
+		  eshell-mode
+		  git-rebase-mode
+		  erc-mode
+		  circe-server-mode
+		  circe-chat-mode
+		  circe-query-mode
+		  sauron-mode
+		  term-mode))
+    (add-to-list 'evil-emacs-state-modes mode)))
+
 ;; Vim Mode
 (use-package evil
   :ensure t
   :init
   (setq evil-want-integration t)
   (setq evil-want-keybinding nil)
+  ; (setq evil-want-C-u-scroll t)
+  ; (setq evil-want-C-i-jump nil)
+  :hook (evil-mode . lv/evil-hook)
   :config
-  (evil-mode 1))
+  (evil-mode 1) ; turn on Evil mode
+  ; (define-key evil-insert-state-map (kbd "C-g") 'evil-normal-state) ; I think this is the same as just hitting escape when you're not in insert mode
+  ; (define-key evil-insert-state-map (kbd "C-h") 'evil-delete-backward-char-and-join)
+
+  ;; Use visual line motions even outside of visual-line-mode buffers
+  (evil-global-set-key 'motion "j" 'evil-next-visual-line)
+  (evil-global-set-key 'motion "k" 'evil-previous-visual-line)
+
+  (evil-set-initial-state 'messages-buffer-mode 'normal)
+  (evil-set-initial-state 'dashboard-mode 'normal))
 
 (use-package evil-collection
   :after evil
