09ecef4aced4819a8b22da918fac548448ae6012
Luis Victoria <lvictoria7@bloomberg.net>
2025-01-30 14:01:55 -0500

    Have config live inside of `src/`

--- init.el
+++ init.el
@@ -8,4 +8,4 @@
 
 ;; Load the configuration from config.org
 (require 'org)
-(org-babel-load-file (expand-file-name "config.org" user-emacs-directory))
+(org-babel-load-file (expand-file-name "src/main.org" user-emacs-directory))
